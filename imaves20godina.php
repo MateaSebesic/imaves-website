5dfe9ae96938e14e65044f7640bc45a76a546662
mmatokovi <mislav.mat@hotmail.com>
2022-04-15 10:32:31 +0200

    msg

---
+++ imaves20godina.php
@@ -0,0 +1,125 @@
+<?php
+include "components/header.php";
+include "components/navigation.php";
+?>
+
+<div class="container" style="margin-top: 140px">
+    <h1>Kroz dva desetljeća poslovanja, bilo je brojnih uspona i padova, raznih poslovnih suradnji, aktivnosti, edukacija, projekata</h1>
+    <br>
+    <h3 style="color:#FF5800;">Osvrnut ćemo se na najvažnije u proteklih 20 godina poslovanja i važnih trenutaka.
+Kako su ljudski resursi najvažniji faktor, pa u tom duhu i njegujemo naše poslovanje.
+Osvrnut ćemo se na najvažnije u proteklih 20 godina poslovanja i važnih trenutaka.
+Kako su ljudski resursi najvažniji faktor, pa u tom duhu i njegujemo naše poslovanje.
+</h3>
+<br>
+      <div class="timeline">
+        <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Početak poslovanja </h3>
+            <p>2002. godina, Vlasnik i direktor Milan Vesel, mag. inf.</p>
+            <p>Početkom poslovanja direktor je sam krenuo u poslovnu avanturu 
+Kako su godine prolazile, tako se i Imaves razvijao
+</p>
+<p>Prilikom razvijanja poslovanja, bez ljudskih resursa nema napretka
+Imaves zapošljava nove suradnike, pa se iz godine u godinu povećavao broj zaposlenih
+</p>
+          </div>
+          <div class="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
+            <img src="assets/images/Picture1.gif" class="img-fluid" alt="img">
+          </div>
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+              <img src="assets/images/Picture3.jpg" alt="" width="230" height="150">
+          </div>
+        </div>
+        <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Početak fokusa na IT riješenja</h3>
+            <p>Kao jedna od vodećih tvrtki s područja ICT razvoja na području Republike Hrvatske predstavljali smo najnovije mogućnosti razvoja i unapređenja poslovanja naših korisnika</p>
+            <p>Obzirom da su računala i računalni programi  nezaobilazni dio radne okoline u današnjem društvu, postizanje ciljeva je moguće ostvariti uvođenjem novih softverskih rješenja koja će olakšati poslovanje, tim putem je krenuo i Imaves.</p>
+          </div>
+          <div class="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
+            <img src="assets/images/pic3.png" class="img-fluid" alt="img">
+          </div>
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time>2003. godine</time>
+            <br>
+            <img src="assets/images/Picture10.png" class="img-fluid" width="160" height="100" alt="img">
+            <img src="assets/images/Picture16.png" class="img-fluid" width="160" height="100" alt="img">
+            <img src="assets/images/Picture14.png" class="img-fluid" width="140" height="70" alt="img">
+            <img src="assets/images/Picture15.png" class="img-fluid" width="150" height="70" alt="img">
+          </div>
+        </div>
+        <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Imaves je davne 2005.godine prepoznao BMC software kao strateškog partnera i od tada ima uspješnu suradnju</h3>
+            <p>BMC Software Inc. je američko poduzeće koje je prema „ITSM Gartner magic quadrant-u“ posljednjih nekoliko godina vodeći ponuđač rješenja za upravljanje IT uslugom</p>
+            <p>Desetci tisuća organizacija, od malih do velikih, diljem svijeta koriste BMC rješenja</p>
+          </div>
+          <div class="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
+            <img src="assets/images/Picture4.png" class="img-fluid" alt="img">
+          </div>
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time>2005. godine</time>
+            <br>
+            <img src="assets/images/Picture9.png" class="img-fluid" width="160" height="100" alt="img">
+            <img src="assets/images/Picture13.png" class="img-fluid" width="160" height="100" alt="img">
+          </div>
+        </div>
+        <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Certification ITIL</H3>
+            <p>
+ITIL certifikati omogućavaju IT stručnjacima najbolja rješenja u poslovima upravljanja IT uslugama. ITIL-ov okvir pomaže poboljšati IT poslovanje bez obzira na to gdje se organizacija nalazi u životnom ciklusu upravljanja uslugama, a pomaže tvrtkama smanjujući troškove uz istovremeno poboljšanje IT usluga. Imaves je tvrtka koja teži poboljšanju, te svoje zaposlenike u svojim odjelima i potrebnim certifikatima šalje na edukacije.</p>
+          </div>
+          <div class="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
+            <img src="assets/images/Picture6.png" class="img-fluid" alt="img">
+          </div>
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time>2005. godine</time>
+          </div>
+        </div>
+        <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Partner PriceFx</H3>
+            <p>PriceFx je vodeće cloud-based rješenje za upravljanje cijenama i troškovima vaših proizvoda i usluga. PriceFx vam omogućuje izradu cjenika i ponuda, analizu poslovanja, optimizaciju cijena te upravljanje ugovorima, distribucijskim kanalima i povratima kroz pristupačno korisničko sučelje. Zahvaljujući visokoj razini podesivosti može se prilagoditi individualnim potrebama svakoga korisnika i pomoći mu da poveća učinkovitost svog poslovanja i ostvari povrat ulaganja. Možemo se pohvaliti kako smo jedini partneri Pricefx-a u Hrvatskoj koji nudi rješenje  takvog softvera. Pricefx je tvrtka koja je predana isporuci najboljeg softvera za određivanje cijena u klasi kako bismo povećali profitabilnost i povećali tržišni udio primatelja usluge.</p>
+          </div>
+          <div class="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
+            <img src="assets/images/Picture7.png" class="img-fluid" alt="img">
+          </div>
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time>2019. godine</time>
+          </div>
+        </div>
+        <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Partner 4me</H3>
+            <p>4me® je ESM (enterprise service management) rješenje za besprijekornu suradnju između unutarnjih i vanjskih timova.
+4me je jedino ESM rješenje koje omogućuje svim unutarnjim odjelima poput IT-a, HR-a, financija i održavanja jednostavnu i efikasnu suradnju. Ista razina suradnje omogućena je i između korisnika i vanjskog pružatelja usluga kojima su neke usluge dodijeljene (outsourced). Za poslovne korisnike, 4me je aplikacija preko koje uvijek mogu dobiti pomoć. Imaves stručni tim odjela 4me, pruža svu potrebnu podršku svojim korisnicima. Svakako neskromno možemo kazati kako smo ponosni na naše IT stručnjake.</p>
+          </div>
+          <div class="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
+            <img src="assets/images/Picture18.png" width="50" height="50" class="img-fluid" alt="4me">
+          </div>
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time>2019. godine</time>
+            <br>
+            <img src="assets/images/Picture12.png" class="img-fluid" width="160" height="100" alt="img">
+            <br>
+          </div>
+        </div>
+        <div class="row no-gutters justify-content-end justify-content-md-around align-items-start  timeline-nodes">
+          <div class="col-10 col-md-5 order-3 order-md-1 timeline-content">
+            <h3 class=" text-light">Neupitna stručnost uz neograničenu zabavu!</H3>
+            <p>Svakim danom u svakom pogledu sve više napredujemo!Ukoliko vam se sviđa naša tvrtka i prepoznali ste se baš u ovakvom okruženju, pogledajte naše otvorene pozicije na web stranici.</p>
+          </div>
+          <div class="col-2 col-sm-1 px-md-3 order-2 timeline-image text-md-center">
+            <img src="assets/images/Picture100.gif" class="img-fluid" alt="img">
+          </div>
+          <div class="col-10 col-md-5 order-1 order-md-3 py-3 timeline-date">
+            <time>2022. godine</time>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+<?php include "components/footer.php";?>
